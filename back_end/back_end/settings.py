73a9265c3911e050b263f7761adca0b032621685
Amos V <amosvernon.dev@gmail.com>
2024-02-06 18:53:01 +0000

    update settings py

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -82,13 +82,25 @@ WSGI_APPLICATION = 'back_end.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+
+## Postgres
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME':'property-db',
+         'USER':'postgres',
+         'PASSWORD':'x',
+         'HOST':'property-db.crikyc02wgmk.eu-west-2.rds.amazonaws.com',
+         'PORT':'5432'
+     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
