6a620e53ab69b65bf3a1bc2dc23eeb04ae3b1365
Amos V <amosvernon.dev@gmail.com>
2024-02-09 19:20:47 +0000

    temporarily allow all hosts

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=atft10cqs$yl!m-%l$8qv2*9olrhz&gjrl5z42lt8dqoqs8ek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
