101ba4f529a5d682d6008a96e57c2af274304f5a
Alex <alexanderwjames26@gmail.com>
2024-02-09 19:25:32 +0000

    pushing up auth file

--- vite-ui/src/components/auth.js
+++ vite-ui/src/components/auth.js
@@ -1,4 +1,3 @@
-import { useAppContext } from "../App.context";
 import apiLocation from "./apiLocation";
 
 function getCurrentDate() {
@@ -6,17 +5,14 @@ function getCurrentDate() {
     const year = today.getFullYear();
     const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
     const day = today.getDate().toString().padStart(2, '0');
-  
     const formattedDate = `${year}-${month}-${day}`;
     return formattedDate;
   }
 
 export default function generateAuthToken(){
-    const { currentUser } = useAppContext()
+  const date = getCurrentDate()
 
-    const date = getCurrentDate()
+  const key = btoa(`for:${apiLocation}:authon${date}`)
 
-    const key = btoa(`${currentUser}:for${apiLocation}:authon${date}`)
-
-    return key
+  return key
 }
