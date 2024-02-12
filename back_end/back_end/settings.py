c36e37991e21ad518689ead6588b35b1889c24cf
Amos V <amosvernon.dev@gmail.com>
2024-02-12 18:27:32 +0000

    add csrf trusted origins

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -26,10 +26,10 @@ SECRET_KEY = 'django-insecure-=atft10cqs$yl!m-%l$8qv2*9olrhz&gjrl5z42lt8dqoqs8ek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "https://ajo-property-manager.co.uk"]
 # ALLOWED_HOSTS = ['http://www.ajo-property-manager.co.uk:8000', 'http://127.0.0.1:8000']
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["*", "https://ajo-property-manager.co.uk"]
 
 # Application definition
 
